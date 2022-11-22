95533e58a5a8f64ddb232a6d0b1dfa183b737bba
volky13 <volkanekici12@gmail.com>
2022-11-22 23:45:16 +0300

    update v5

---
+++ app_api/models/db.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+var dbUrl ="mongodb://localhost/mekanbul"
+//var dbUrl ="mongodb+srv://volkan123:volkan123@mekanbul.0zzopxd.mongodb.net/mekanbul?retryWrites=true&w=majority";
+require("./mekansema");
+mongoose.connect(dbUrl);
+
+mongoose.connection.on("connected", () => {
+    console.log(dbUrl + " adresindeki veri tabanına bağlandı");
+});
+
+mongoose.connection.on("error", () => {
+    console.log("Bağlantı hatası");
+});
+
+mongoose.connection.on("disconnected", () => {
+    console.log("Bağlantı kesildi");
+});
+
+function close(msg, callback) {
+    mongoose.connection.close(() => {
+        console.log(msg);
+        callback();
+    });
+}
+process.on("SIGINT", () => {
+    close("Uygulama kapatilidi", () => {
+        process.exit(0);
+    });
+});
